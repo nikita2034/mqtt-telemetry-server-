8d6207d4e9f7b62fc362a63939681c9b5318ca9e
Vasya <asteam_vasya@maz.by>
2025-01-18 14:11:59 +0300

    rename host of db

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 // Настройки подключения
 const sequelize = new Sequelize('client_vehicle_tracking', 'postgres', '1256', {
-    host: 'localhost', // Укажите IP-адрес удаленного сервера
+    host: 'db', // Укажите IP-адрес удаленного сервера
     dialect: 'postgres',   // Используемый диалект (PostgreSQL)
     logging: console.log,        // Отключение логирования SQL-запросов
     port: 5432,            // Порт подключения (обычно 5432 для PostgreSQL)
